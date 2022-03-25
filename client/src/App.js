e13835667b489fda463010c476ad1669e8349ad5
Dylan Marvel <marvel.dylan.james@gmail.com>
2022-03-24 20:02:23 -0500

    added rideDetails and fixed axios calls

--- client/src/App.js
+++ client/src/App.js
@@ -1,48 +1,25 @@
-<<<<<<< HEAD
-import React, { useState } from 'react'
-import { Routes, Route } from 'react-router-dom'
-import AllRides from "./components/AllRides"
-import RideDetails from "./components/RideDetails"
-import RideReview from "./components/RideReviews"
-=======
-import React, { useState, useEffect } from 'react'
+// import React, { useState, useEffect } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import AllRides from './components/AllRides'
 import RideDetails from './components/RideDetails'
 import RideReviews from './components/RideReviews'
-import axios from 'axios'
 
 
->>>>>>> main
+
 
 function App() {
 
-  const [ rides, setRides ] = useState('')
-
-  useEffect(() => {
-    async function getRides() {
-      const res = await axios.get('http://localhost:3001/')
-      setRides(res.data.rides.name)
-      console.log(res.data.rides)
-    }
-    getRides()
-  }, [])  
+  // const [ reviews, setReviews ] = useState('')
 
   return (
-<<<<<<< HEAD
-    <div className="App">
-      <header className="App-header">
-=======
     <div>
       <header>
-        <Routes>
-          <Route path="/" element={ <AllRides /> } />
-        </Routes>
->>>>>>> main
       </header>
       <main>
         <Routes>
-          <Route path="/" element={<AllRides /> } />
+          <Route path="/" element={<AllRides/> } />
+          <Route path="/:id" element={<RideDetails />} />
+          {/* <Route path="/reviews" element={<RideReviews/>} /> */}
         </Routes>
       </main>
     </div>
