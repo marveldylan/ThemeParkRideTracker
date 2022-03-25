c56547c69216cd7031d2327c9952d730bcefa6c2
Dylan Marvel <marvel.dylan.james@gmail.com>
2022-03-25 12:05:10 -0500

    ride details

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,11 +1,9 @@
 const { Router } = require('express');
 const router = Router();
-const rideController = require('../controllers/Controller1.js')
-const reviewController = require('../controllers/Controller2.js')
-
+const rideController = require('../controllers/Ride.js')
+const reviewController = require('../controllers/Review')
 
 router.get('/', rideController.getAllRides)
 router.get('/reviews/:id', reviewController.getAllReviews)
 router.post('/reviews/:id', reviewController.createReview)
-
 module.exports = router;
